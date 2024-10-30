51e60920db1945b340355355cb316cab8df57f4c
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2024-10-30 15:55:26 -0700

    Update new _file.py

--- new _file.py
+++ new _file.py
@@ -1,23 +1,9 @@
 #creating new python file for a test 
-def is_fibonacci(n):
-  """
-  Checks if a number is a Fibonacci number.
-  """
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
-  # A number is Fibonacci if and only if one of the following is a perfect square
-  return is_perfect_square(5 * n**2 + 4) or is_perfect_square(5 * n**2 - 4)
-
-def is_perfect_square(x):
-  """
-  Checks if a number is a perfect square.
-  """
-
-  if x < 0:
-    return False
-
-  sqrt = int(x**0.5)
-  return sqrt**2 == x
-
-# Example usage
-print(is_fibonacci(8))  # True
-print(is_fibonacci(10)) # False
+# Get the 10th Fibonacci number
+print(fibonacci(10))

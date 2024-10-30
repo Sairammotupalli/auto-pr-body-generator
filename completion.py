de1a8a5620703c331472f76139f26c702937c693
Sairammotupalli <83081096+Sairammotupalli@users.noreply.github.com>
2024-10-30 16:27:43 -0700

    Update completion.py

--- completion.py
+++ completion.py
@@ -30,7 +30,7 @@ class Completion:
 
     def _complete_prompt(self) -> str:
         response = self._openai_client.Completion.create(
-            model="text-davinci-003", prompt=self._prompt.text, max_tokens=1024
+            model="gpt-3.5-turbo", prompt=self._prompt.text, max_tokens=1024
         )
         return response.choices[0].text
 
